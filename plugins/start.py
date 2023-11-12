b0041e28263e831f6f9d86f8b2d35ab20fa618b1
Ultroid Official <94180423+7thofficial@users.noreply.github.com>
2023-11-12 09:21:18 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,64 +9,95 @@ from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
+import logging
+import asyncio
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+import secrets
+
+# Set your MongoDB connection URL
+MONGODB_URL = "mongodb+srv://Cluster0:Cluster0@cluster0.c07xkuf.mongodb.net/?retryWrites=true&w=majority"
 
 # Connect to MongoDB
-mongo_client = pymongo.MongoClient(os.getenv("mongodb+srv://Cluster0:Cluster0@cluster0.c07xkuf.mongodb.net/?retryWrites=true&w=majority"))
-db = mongo_client["Cluster0"]
-token_collection = db["tokens"]
-user_collection = db["users"]
+client = MongoClient(MONGODB_URL)
+db = client["Cluster0"]
+tokens_collection = db["tokens"]
 
-def generate_tokens():
-    tokens = [str(random.randint(100000, 999999)) for _ in range(10)]
-    token_collection.insert_many({"token": token} for token in tokens)
+# Set your API ID and API Hash
+API_ID = "22046181"
+API_HASH = "54a82e965ae136785450fc8c2beaaede"
 
+# Token expiration period (1 day in seconds)
+TOKEN_EXPIRATION_PERIOD = 86400
 
-def get_unused_token():
-    unused_tokens = token_collection.find({"user_id": {"$exists": False}})
-    return unused_tokens[0] if unused_tokens else None
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+# Define the start command to generate or verify a token
+def start(update: Update, context: CallbackContext) -> None:
+    user_id = update.message.from_user.id
+    user_token = get_stored_token(user_id)
 
-@Bot.on_message(filters.command("start"))
-async def start_command(client: Client, message: Message):
-    user_id = message.from_user.id
-
-    # Check if the user is already in the database
-    if user_collection.find_one({"user_id": user_id}):
-        await message.reply("You have already started the bot!")
-    else:
-        # Assign a token to the user
-        unused_token = get_unused_token()
-        if unused_token:
-            token = unused_token["token"]
-            user_collection.insert_one({"user_id": user_id, "token": token})
-            await message.reply(f"Welcome! Your token is: `{token}`")
+    if user_token:
+        # Check if the token is expired
+        if is_token_expired(user_id):
+            # Token expired, generate a new one for verification
+            new_token = generate_token(user_id)
+            update.message.reply_text(f"Your previous token has expired. Your new token is: {new_token}")
         else:
-            await message.reply("Sorry, no available tokens at the moment. Try again later.")
-
-
-@Bot.on_callback_query()
-async def handle_callback_query(client: Client, query):
-    user_id = query.from_user.id
-    token = query.data
-
-    # Check if the user has already provided a token
-    if user_collection.find_one({"user_id": user_id}):
-        await query.answer("You have already provided a token.")
+            update.message.reply_text(f"You have a valid token: {user_token}. Use /check to verify.")
     else:
-        # Update the database with the provided token
-        token_entry = token_collection.find_one({"token": token, "user_id": {"$exists": False}})
-        if token_entry:
-            token_collection.update_one({"_id": token_entry["_id"]}, {"$set": {"user_id": user_id}})
-            user_collection.insert_one({"user_id": user_id, "token": token})
-            await query.answer("Token accepted!")
-        else:
-            await query.answer("Invalid token. Please try again.")
+        # No token found, generate a new one for verification
+        new_token = generate_token(user_id)
+        update.message.reply_text(f"Welcome! Your token for verification is: {new_token}")
+
+# Define a command to check user's token
+def check(update: Update, context: CallbackContext) -> None:
+    user_id = update.message.from_user.id
+    user_token = context.args[0] if context.args else None
+
+    # Check if the provided token is valid
+    if is_valid_token(user_id, user_token):
+        update.message.reply_text("Token is valid! Verification successful.")
+        reset_token_verification(user_id)
+    else:
+        update.message.reply_text("Token is invalid. Please check and try again.")
+
+# Function to generate a unique token for a user
+def generate_token(user_id):
+    token = secrets.token_hex(16)
+    expiration_time = datetime.now() + timedelta(seconds=TOKEN_EXPIRATION_PERIOD)
+    tokens_collection.update_one({"user_id": user_id}, {"$set": {"token": token, "expiration_time": expiration_time}}, upsert=True)
+    return token
+
+# Function to check if a token is valid
+def is_valid_token(user_id, user_token):
+    stored_token_info = tokens_collection.find_one({"user_id": user_id})
+    if stored_token_info and stored_token_info["token"] == user_token:
+        return not is_token_expired(user_id)
+    return False
+
+# Function to check if a token is expired
+def is_token_expired(user_id):
+    stored_token_info = tokens_collection.find_one({"user_id": user_id})
+    if stored_token_info:
+        expiration_time = stored_token_info.get("expiration_time")
+        return expiration_time and expiration_time < datetime.now()
+    return True
+
+# Function to reset the token verification process
+def reset_token_verification(user_id):
+    tokens_collection.update_one({"user_id": user_id}, {"$set": {"expiration_time": None}})
+
+# Function to retrieve stored token from MongoDB
+def get_stored_token(user_id):
+    stored_token_info = tokens_collection.find_one({"user_id": user_id})
+    return stored_token_info["token"] if stored_token_info else None
 
 
-@Bot.on_message(filters.command("generate_tokens"))
-async def generate_tokens_command(client: Client, message: Message):
-    generate_tokens()
-    await message.reply("Tokens generated and saved to the database.")
+#=====================================================================================##
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
@@ -253,3 +284,18 @@ Unsuccessful: <code>{unsuccessful}</code></b>"""
         msg = await message.reply(REPLY_ERROR)
         await asyncio.sleep(8)
         await msg.delete()
+
+
+def main():
+    updater = Updater(API_ID, API_HASH, use_context=True)
+    dp = updater.dispatcher
+
+    # Your existing handlers
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("check", check))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
