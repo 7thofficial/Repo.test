5f87b4e4228153822fda6403e6d52378b16c7ed1
Ultroid Official <94180423+7thofficial@users.noreply.github.com>
2023-11-13 14:03:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5415771622"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Cluster0:Cluster0@cluster0.c07xkuf.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Cluster0:Cluster0@cluster0.jsjzuus.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
