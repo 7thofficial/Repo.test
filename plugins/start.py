7dae92b0b8e59f1062c56b1ff1629a0a156b146a
Ultroid Official <94180423+7thofficial@users.noreply.github.com>
2023-11-15 09:51:05 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -62,11 +62,10 @@ async def process_matching_token(client: Client, message: Message):
     if stored_token:
         provided_token = message.command[1] if len(message.command) > 1 else None
 
-        if provided_token == stored_token:
+        if await verify_token(user_id, provided_token)
             # Token matches, proceed with the action
             print("Token matched.")
-            await verify_token(user_id, provided_token, True, message)  # Save token match status in the database
-            # Your further logic here
+             
         else:
             # Token didn't match, reply and request verification
             new_token = await generate_24h_token(user_id, tokens_collection)
