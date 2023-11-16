bdabc9655710f0bd308556ca3d75b913d4389913
Ultroid Official <94180423+7thofficial@users.noreply.github.com>
2023-11-16 13:17:16 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,7 +9,7 @@ import aiohttp
 import requests
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from motor import motor_asyncio
 
