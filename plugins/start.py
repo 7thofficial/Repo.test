2834b199964c2946c98f7e086bbc639a585e0147
Ultroid Official <94180423+7thofficial@users.noreply.github.com>
2023-12-10 07:05:31 +0000

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -14,7 +14,7 @@ from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from motor import motor_asyncio
 from bot import Bot
 from config import (
-    DB_URI, DB_NAME, ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT ,DbManager
+    DB_URI, DB_NAME, ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 )
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
@@ -28,6 +28,66 @@ SHORT_URL = "vnshortener.com"
 SHORT_API = "d20fd8cb82117442858d7f2acdb75648e865d2f9"
 # Token expiration period (1 day in seconds)
 TOKEN_EXPIRATION_PERIOD = 86
+
+class DbManager:
+    def __init__(self):
+        self.__err = False
+        self.__db = None
+        self.__conn = None
+        self.__connect()
+
+    def __connect(self):
+        try:
+            self.__conn = AsyncIOMotorClient(DB_URI)
+            self.__db = self.__conn.z
+        except PyMongoError as e:
+            LOGGER.error(f"Error in DB connection: {e}")
+            self.__err = True
+
+    async def update_config(self, dict_):
+        if self.__err:
+            return
+        await self.__db.settings.config.update_one({'_id': bot_id}, {'$set': dict_}, upsert=True)
+        self.__conn.close
+      
+    async def update_user_tdata(self, user_id, token, time):
+        if self.__err:
+            return
+        await self.__db.access_token.update_one({'_id': user_id}, {'$set': {'token': token, 'time': time}}, upsert=True)
+        self.__conn.close
+
+    async def update_user_token(self, user_id, token):
+        if self.__err:
+            return
+        await self.__db.access_token.update_one({'_id': user_id}, {'$set': {'token': token}}, upsert=True)
+        self.__conn.close
+
+    async def get_token_expire_time(self, user_id):
+        if self.__err:
+            return None
+        user_data = await self.__db.access_token.find_one({'_id': user_id})
+        if user_data:
+            return user_data.get('time')
+        self.__conn.close
+        return None
+
+    async def get_user_token(self, user_id):
+        if self.__err:
+            return None
+        user_data = await self.__db.access_token.find_one({'_id': user_id})
+        if user_data:
+            return user_data.get('token')
+        self.__conn.close
+        return None
+
+    async def delete_all_access_tokens(self):
+        if self.__err:
+            return
+        await self.__db.access_token.delete_many({})
+        self.__conn.close
+
+if DB_URI:
+    bot_loop.run_until_complete(DbManager().db_load())
 
 def get_readable_time(seconds):
     periods = [('d', 86400), ('h', 3600), ('m', 60), ('s', 1)]
